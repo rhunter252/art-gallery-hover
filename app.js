281ead75c0009c25904f17205c59ec56618d16bc
Randy Hunter <randyh252@gmail.com>
2023-04-06 18:44:54 -0400

    setting up backend

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello");
+const express = require("express");
+const dotenv = require("dotenv");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/openai", require("./routes/openaiRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
